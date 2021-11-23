9942d3d4943ead89b55b35791814607c8e8d5869
sham <shamlo_redzx@yahoo.com>
2021-11-24 00:00:52 +0300

    Learning about Callbacks

---
+++ playground/3-callback.ts
@@ -0,0 +1,17 @@
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (num1: number, num2: number, callback: (num: number) => void) => {
+    setTimeout(() => {
+        callback(num1 + num2);
+    }, 2000);
+};
+
+add(1, 4, (sum) => {
+    console.log(sum); // Should print: 5
+});
