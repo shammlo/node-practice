136e1c67c753fee9c3b08f855e9419b130a8c2ac
sham <shamlo_redzx@yahoo.com>
2021-12-04 22:02:23 +0300

    Uisng Express.js

---
+++ web-server/src/app.ts
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+
+import { Request, Response } from 'express';
+const app = express();
+
+const port = 3000;
+
+const publicDirectoryPath = path.join(__dirname, '../public');
+const aboutDirectoryPath = path.join(__dirname, '../public/about.html');
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello Express');
+});
+
+app.get('/about', (req: Request, res: Response) => {
+    res.sendFile(aboutDirectoryPath);
+});
+
+app.get('/help', (req: Request, res: Response) => {
+    res.sendFile(publicDirectoryPath + '/help.html');
+});
+app.get('/weather', (req: Request, res: Response) => {
+    res.send('Weather page');
+});
+app.listen(port, () => console.log(`Listening on port ${port}`));
