3f2d6afc3fef9207d39206d7817f9d77872028bb
sham <shamlo_redzx@yahoo.com>
2021-12-06 22:02:21 +0300

    Express.js Route

--- web-server/src/app.ts
+++ web-server/src/app.ts
@@ -10,21 +10,20 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 const aboutDirectoryPath = path.join(__dirname, '../public/about.html');
 app.use(express.static(publicDirectoryPath));
 
+app.set('view engine', 'hbs');
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello Express');
+    res.render('index', {
+        title: 'Home Page',
+        name: 'Home',
+    });
 });
 
 app.get('/about', (req: Request, res: Response) => {
-    res.sendFile(aboutDirectoryPath);
+    res.render('index', {
+        title: 'About Page',
+        name: 'About',
+    });
 });
 
-app.get('/help', (req: Request, res: Response) => {
-    res.sendFile(publicDirectoryPath + '/help.html');
-});
-app.get('/weather', (req: Request, res: Response) => {
-    res.send('Weather page');
-});
-app.get('/welcome', (req: Request, res: Response) => {
-    res.send('welcome page');
-});
 app.listen(port, () => console.log(`Listening on port ${port}`));
