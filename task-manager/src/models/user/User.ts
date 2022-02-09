7cc3f5a82fd21084de23347bc3abbc2deae0c84f
sham <shamlooo@yahoo.com>
2022-02-09 21:50:04 +0300

    Adding Schema and pre('save')

--- task-manager/src/models/user/User.ts
+++ task-manager/src/models/user/User.ts
@@ -1,8 +1,9 @@
 export {};
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         trim: true,
@@ -41,5 +42,16 @@ const User = mongoose.model('User', {
         },
     },
 });
+
+userSchema.pre('save', async function (this: typeof userSchema, next: any) {
+    const user = this;
+    console.log(user);
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
