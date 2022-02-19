a3c3b4cc8b555073e41a72fe1b96850da90b58f5
sham <shamlooo@yahoo.com>
2022-02-19 22:02:11 +0300

    Deleting a log

--- task-manager/src/models/user/User.ts
+++ task-manager/src/models/user/User.ts
@@ -45,7 +45,6 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function (this: typeof userSchema, next: any) {
     const user = this;
-    console.log(user);
     if (user.isModified('password')) {
         user.password = await bcrypt.hash(user.password, 8);
     }
