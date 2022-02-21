b02c6c47e373dd8fe48f9bf0e4316f7490dd85a8
sham <shamlooo@yahoo.com>
2022-02-21 22:54:51 +0300

    Removing express middle ware from this file

--- task-manager/src/index.ts
+++ task-manager/src/index.ts
@@ -1,17 +1,28 @@
 const express = require('express');
-// import { Response, Request } from 'express';
-// import { UserType, TaskType } from './util/types/Types';
+const jwt = require('jsonwebtoken');
 require('./database/mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const userRouter = require('./routers/User/UserRouter');
 const taskRouter = require('./routers/Task/TaskRouter');
+
+// import { Request, Response, NextFunction } from 'express';
 // ----------------------------------------------------------------
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // -------------------------------
 // *** APP.USE() ***
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//     if (req.method) {
+//         res.status(503).send('Service Unavailable');
+//     }
+//     next();
+// });
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -21,3 +32,13 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, process.env.secretToUseJWT, { expiresIn: '7 days' });
+
+    const data = jwt.verify(token, process.env.secretToUseJWT);
+
+    return data;
+};
+
+myFunction();
