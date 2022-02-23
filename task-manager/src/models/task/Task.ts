fc78a76c27c736e2b610fbc5ee69b1771c4d2206
sham <shamlooo@yahoo.com>
2022-02-23 21:36:13 +0300

    Adding User database reference

--- task-manager/src/models/task/Task.ts
+++ task-manager/src/models/task/Task.ts
@@ -10,6 +10,11 @@ const taskSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
 });
 
 // taskSchema.pre('save', async function (this: typeof taskSchema, next: any) {
