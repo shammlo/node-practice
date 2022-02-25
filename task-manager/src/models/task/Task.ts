691ad81f3d26e2651367eb3cde8d779aa2ba0d3c
sham <shamlooo@yahoo.com>
2022-02-25 22:59:40 +0300

    Adding timestamps option to schema

--- task-manager/src/models/task/Task.ts
+++ task-manager/src/models/task/Task.ts
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
 
-const taskSchema = new mongoose.Schema({
-    description: {
-        type: String,
-        trim: true,
-        required: true,
+const taskSchema = new mongoose.Schema(
+    {
+        description: {
+            type: String,
+            trim: true,
+            required: true,
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User',
+        },
     },
-    completed: {
-        type: Boolean,
-        default: false,
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User',
-    },
-});
+    { timestamps: true }
+);
 
 // taskSchema.pre('save', async function (this: typeof taskSchema, next: any) {
 //     // const task = this;
--- task-manager/src/models/user/User.ts
+++ task-manager/src/models/user/User.ts
@@ -4,54 +4,57 @@ const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Task = require('../task/Task');
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-    },
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+        },
 
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        validate(value: string) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        },
-    },
-    password: {
-        type: String || Number,
-        required: true,
-        minlength: 8,
-        trim: true,
-        validate(value: string) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value: number) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        },
-    },
-    tokens: [
-        {
-            token: {
-                type: String,
-                required: true,
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            validate(value: string) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid');
+                }
             },
         },
-    ],
-});
+        password: {
+            type: String || Number,
+            required: true,
+            minlength: 8,
+            trim: true,
+            validate(value: string) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error('Password cannot contain "password"');
+                }
+            },
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value: number) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number');
+                }
+            },
+        },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+    },
+    { timestamps: true }
+);
 
 userSchema.virtual('tasks', {
     ref: 'Tasks',
