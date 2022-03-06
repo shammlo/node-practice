96a3c577dbc868fac12e81631e6feb7950c73dc4
sham <shamlooo@yahoo.com>
2022-03-06 22:32:32 +0300

    Adding seperate .env file for test cases

--- task-manager/jest.config.js
+++ task-manager/jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+require('dotenv').config({ path: './test.env' });
+
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    setupFiles: ['dotenv/config'],
 };
